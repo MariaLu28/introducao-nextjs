742949cf62f6f6f52cbdfb9678d824a4f624a2bf
MariaLu28 <161661519+MariaLu28@users.noreply.github.com>
2024-08-06 20:40:45 -0300

    Aula 06/08
    Usuarios, cadastro, login

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -4,11 +4,13 @@ import styles from "./page.module.css";
 
 export default function Home() {
   return (
-    <div>
-       <h1>Aula Next JS </h1>
-       <h2>Exemplo h2</h2>
+    <div className="containerGlobal">
+      <h1 className={styles.tituloHomePage}>Aula Next JS </h1>
+      <h2>Exemplo h2</h2>
+      <h3>Exemplo h3</h3>
+      <p>Texto Parágrafo</p>
 
     </div>
-  
+
   );
 }
